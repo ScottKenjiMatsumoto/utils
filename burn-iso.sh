14c00078aa4bba996af1c3289f4a3797727c2dc1
ScottKenjiMatsumoto <scottkenjimatsumoto@gmail.com>
2026-02-05 20:03:49 -0500

    burn iso for usb

--- burn-iso.sh
+++ burn-iso.sh
@@ -3,5 +3,9 @@
 ISO_PATH=$1 # ~/Downloads/debian.iso
 OUTPUT_DEVICE=$2 # /dev/sda
 
-sudo dd if=$ISO_PATH of=$OUTPUT_DEVICE bs=4M status=progress
+# (oflag=direct) direct i/o to bypass system cache
+# (conv=fdatasync) synchronized data/metadata writes
+sudo dd if=$ISO_PATH of=$OUTPUT_DEVICE bs=4M status=progress \
+  oflag=direct \
+  conv=fdatasync
 sudo sync
